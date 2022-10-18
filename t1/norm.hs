4c4a478117021eccced5276e1560a9e45dc57783
AntonioMatosGitHub <vaziosmellsbad@gmail.com>
2022-10-18 12:26:31 +0100

    dá para multiplicar monómios, mas n ão polinomios

--- t1/norm.hs
+++ t1/norm.hs
@@ -35,31 +35,61 @@ addSome x y = (fst x + fst y, snd y)
 addMons :: Pol -> Pol
 addMons[] = []
 addMons (x:xs) | findX x (xs) = addMons $ (addSome x (head xs)) : tail xs
-               | otherwise   = x : addMons xs
-
--- soma dois polinómios
-addPols :: Pol -> Pol -> Pol
-addPols x y = addMons(x ++ y)
+               | otherwise    = x : addMons xs
 
 -- normaliza polinómio
 normalize :: Pol -> Pol
 normalize p = removeZero $ addMons $ sortPol p
 
+-- soma dois polinómios
+addPols :: Pol -> Pol -> Pol
+addPols x y = normalize(x ++ y)
+
+-- (auxílio do multMons) descobre se o monómio tem algum var x que possa ser somado ao var n
+findVar :: Var -> [Var] -> Bool
+findVar _ [] = False
+findVar n (x:xs)
+  | fst n == fst x = True
+  | otherwise      = findVar n xs
+
+-- (auxílio do multMons) soma expoentes de 2 vars
+addExp :: Var -> Var -> Var
+addExp x y = (fst y, snd x + snd y)
+
+-- (auxílio do multMons) multiplica vars compatíveis de um monómio
+multVars :: [Var] -> [Var]
+multVars[] = []
+multVars (x:xs) | findVar x (xs) = multVars $ (addExp x (head xs)) : tail xs
+                | otherwise      = x : multVars xs
+
+-- multiplica monómios
+multMons :: Mon -> Mon-> Mon
+multMons x y = (fst x * fst y, multVars $ sort(snd x ++ snd y))
 
 -- valores aleatórios usados em testes
-v1,v2,v3,v4 :: Var
+v1,v2,v3,v4,v5,v6,v7 :: Var
 m1,m2,m3 :: Mon
-
 p1 :: Pol
+
 v1 = ('a', 1)
 v2 = ('a', 2)
 v3 = ('a', 3)
 v4 = ('a', 1)
+v5 = ('b', 2)
+v6 = ('b', 4)
+v7 = ('c', 1)
 m1 = (3, sort[v1])
 m2 = (2, sort[v2])
 m3 = (4, sort[v3])
 m4 = (1, sort[v4])
+m5 = (2, sort[v6, v2, v7]) -- 2 a^2 b^4 c
+m6 = (3, sort[v7, v3, v5]) -- 3 a^3 b^2 c
+                           -- 6 a^5 b^6 c^2
 
 p1 = [m1, m2, m3, m4]
 
-normalize_mon = removeZero $ addMons $ sortPol p1
+normalize_mon = normalize p1
+multa = multMons m5 m6
+
+--multPol :: Pol -> Pol -> Pol
+--multPol x y = normalize $
